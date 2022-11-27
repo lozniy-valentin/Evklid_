1af5af86a6351ecbdd40961e47393afe7b801675
lozniy-valentin <lozniy89gmail.com>
2022-11-28 00:04:58 +0300

    adaptive_1024

--- JS/script.js
+++ JS/script.js
@@ -32,5 +32,20 @@ $( function() {
 });
 } );
 
+/*burger*/
+const burgerBtn = document.querySelector('.burger');
+const menuClose = document.querySelector('.menu-close');
+const menuBurger = document.querySelector('.nav');
+
+burgerBtn.addEventListener('click', () => {
+  menuBurger.classList.add('burger-active');
+});
+
+menuClose.addEventListener('click', () => {
+  menuBurger.classList.remove('burger-active');
+});
+
+
+
 
 
